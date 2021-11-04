cb7871bf976bb5dc926ee69a549015e3be0f4f29
nuramankg00 <nursultanamanaiev24@gmail.com>
2021-11-04 21:04:58 +0600

    update

--- js/script.js
+++ js/script.js
@@ -59,3 +59,17 @@ const swiper = new Swiper(".project-slider", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+const openMobileMenuBtn = document.querySelector(".device-menu");
+const headerMenu = document.querySelector(".header-nav");
+
+openMobileMenuBtn.addEventListener("click", () => {
+    if(openMobileMenuBtn.classList.contains("open")) {
+        openMobileMenuBtn.classList.remove("open");
+        headerMenu.classList.remove("active");
+    } else {
+        headerMenu.classList.add("active");
+        openMobileMenuBtn.classList.add("open");
+    }
+})
+
